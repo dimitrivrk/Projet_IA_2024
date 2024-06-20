e5688758c2b337b42927171258ed2e4944d9abba
Oonilink <nicolas.beunon@isen-ouest.yncrea.fr>
2024-06-20 12:02:00 +0200

    nicob commit 4

--- feature2/script.py
+++ feature2/script.py
@@ -3,22 +3,36 @@ import numpy as np
 from predictionAge import load_and_preprocess_data
 from json import loads, dumps
 import joblib
+from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder, StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neural_network import MLPRegressor
+from sklearn.cross_decomposition import PLSRegression
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
 
 
 X_train, X_test, y_train, y_test = load_and_preprocess_data()
+test_data = pd.concat(X_train)
 
 
 def dico():
     # CREATION DU FICHIER PLK
     dico = {
-        'or': ordinal_encoder,
-        'oh': onehot_encoder,
-        'ss': ss,
-        'rf': rfr,
-        # 'dt': dtr,
-        # 'ml': mlp,
-        # 'pl': pls,
+        'or': OrdinalEncoder(categories=[['Jeune', 'Adulte', 'vieux', 'senescent']]),
+        'oh': OneHotEncoder(sparse_output=False),
+        'ss': StandardScaler(),
+        'rf': RandomForestRegressor(n_estimators=200,
+                                min_samples_split=2,
+                                max_samples=0.8,
+                                max_leaf_nodes=150,
+                                max_depth=32,
+                                random_state=42),
+        # 'dt': dtr = DecisionTreeRegressor(max_depth=12,
+        #                                 min_impurity_decrease=0.1,
+        #                                 min_samples_leaf=3,
+        #                                 min_samples_split=4),
+        # 'ml': mlp = MLPRegressor(hidden_layer_sizes=(100, 100, 100), max_iter=1000),
+        # 'pl': pls = PLSRegression(n_components=4),
     }
     with open('fichier_joblib.pkl', 'wb') as file:
         joblib.dump(dico, file)
@@ -28,23 +42,40 @@ def dico():
 def script(JSON_fichier, dico):
 
     new_data = pd.read_csv(JSON_fichier)
-    print(new_data['fk_nomtech'])
+    print(new_data[['fk_nomtech']])
 
     dico = joblib.load('fichier_joblib.pkl')
 
-    new_data[["fk_stadedev"]] = dico['or'].transform(new_data[["fk_stadedev"]])
-    new_data[["fk_nomtech"]] = dico['oh'].transform(new_data[["fk_nomtech"]])
-
-    new_data = dico['ss'].transform(new_data)
-
-    X = new_data[['haut_tronc', 'tronc_diam', 'fk_stadedev', 'feuillage', 'fk_nomtech', 'clc_nbr_diag']]
+    X = new_data[['haut_tronc', 'tronc_diam', 'fk_stadedev', 'feuillage', 'fk_nomtech', 'clc_nbr_diag']].copy()
     Y = new_data[['age_estim']]
+
+    X[["fk_stadedev"]] = dico['or'].transform(new_data[["fk_stadedev"]])
+
+    feuillage = dico['oh'].transform(X[["feuillage"]])
+    feuillage_df = pd.DataFrame(feuillage, columns=dico['oh'].get_feature_names_out(["feuillage"]))
+    #fk_nomtech = dico['oh'].transform(X[["fk_nomtech"]])
+    #fk_nomtech_df = pd.DataFrame(fk_nomtech, columns=dico['oh'].get_feature_names_out(["fk_nomtech"]))
+
+
+    X.drop(columns=["feuillage"], inplace=True)
+    X = pd.concat([X, feuillage_df], axis=1)
+
+
+    #X[["haut_tronc"]] = dico['ss'].fit_transform(X[["haut_tronc"]])
+    #X[["tronc_diam"]] = dico['ss'].fit_transform(X[["tronc_diam"]])
+    #X[["fk_stadedev"]] = dico['ss'].fit_transform(X[["fk_stadedev"]])
+    X[["feuillage"]] = dico['ss'].transform(X[["feuillage"]])
+    #X[["fk_nomtech"]] = dico['ss'].fit_transform(X[["fk_nomtech"]])
+    #X[["clc_nbr_diag"]] = dico['ss'].fit_transform(X[["clc_nbr_diag"]])
+
+    #X = dico['ss'].transform(X)
+
+
 
     print(dico['rf'])
 
-    rfr_precision = r2_score(y_test, rfr_prediction)
-    rfr_rmse = np.sqrt(mean_squared_error(y_test, rfr_prediction))
-    rfr_mae = mean_absolute_error(y_test, rfr_prediction)
+    rfr_prediction = dico['rf'].predict(X_test)
+    print(rfr_prediction)
 
 
 script("Data_Arbre.csv", dico)
